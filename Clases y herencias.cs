6a2fd85ffea58c05bef490f3dc8f5d10603a4544
Samanthadlila <151056621+Samanthadlila@users.noreply.github.com>
2024-02-13 13:29:46 -0600

    Primera semana
    Codigo mal implementado

---
+++ Clases y herencias.cs
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Transporte{
+    private: //Atributos
+        string color;
+        string nombre;
+    
+    public://Metodos
+         Transporte(string, string);
+           void desplazamiento();
+};
+//Inicializar el Constructor
+Transporte::Transporte(string _color, string _nombre){
+    color = _color;
+    nombre = _nombre;
+}
+
+void Trasnporte::desplazamiento(){
+    cout << "Transporte: " << nombre << " se esta desplazando" << end;
+
+}
+
+int main()
+{
+
+    Transporte Carro = Transporte("Azul", "Toyota");
+
+    Carro.desplazamiento();
+
+
+    return 0;
+}
